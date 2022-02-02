451f33a58055dad7c02a1488cda46cd29ca0f22b
evansamenorfe <evansamenorfe@gmail.com>
2022-02-02 23:04:51 +0000

    media quaries

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -6,7 +6,7 @@ const month = ["January","February","March","April","May","June","July","August"
 let day = weekday[d.getDay()];
 let monthname = month[d.getMonth()];
 
-document.querySelector(".currentyear").textContent += `\u00A9 ${year} GS Chamber`;
+document.querySelector(".currentyear").textContent += `\u00A9 ${year} Wheb Media`;
 
 const lastupdate = document.querySelector(".lastupdated");
 lastupdate.textContent += `Last Modification: ${document.lastModified}`;
